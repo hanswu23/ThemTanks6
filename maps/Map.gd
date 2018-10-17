262ce6f1e821f0298c891f0dfe694166145ddeab
hanswu23 <hanswu23@hotmail.com>
2018-10-16 20:26:21 -0700

    Added a second tank

--- maps/Map.gd
+++ maps/Map.gd
@@ -16,3 +16,6 @@ func _on_Tank_shoot(bullet,_position,_direction):
 	var b = bullet.instance()
 	add_child(b)
 	b.start(_position, _direction)
+
+func on_tank_shoot():
+	pass # replace with function body
